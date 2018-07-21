e70a342fa1d8bdee5124a7dff5e1e5adc7bb2a31
Ignas Daukšas <ignasd@gmail.com>
2018-07-21 13:37:00 +0300

    v2.0.0-alpha

--- rec.bat
+++ rec.bat
@@ -1,24 +1,40 @@
 @echo off
 cd /d %~dps0
 cls
-title FripTV
+title FripTV 0.18 rec script v2-alpha
 color 17
-if "%1"=="planinis" goto planinis
-if exist friptv.exe goto nustatymai
+if exist friptv.exe goto pradzia
 echo Nerastas friptv.exe
 goto isejimas
-:nustatymai
+:pradzia
+if "%1"=="planinis" goto planinis
 set /p kanalas=Kanalo numeris: 
 set /p trukme=Iraso trukme (minutemis): 
 set /p failas=Failo pavadinimas (su galune): 
 set /p isjungimas=Isjungti kompiuteri po irasymo? [T/N]: 
 set /p pradeti=Pradeti dabar? [T/N]: 
 if /i "%pradeti%"=="T" goto irasymas
-set /p diena=Kelinta diena pradeti? (jeigu siandien, nerasykite nieko): 
-set /p laikas=Kokiu laiku pradeti? (formatas HH:mm): 
-if "%diena%"=="" (set data=%laikas% /interactive) else (set data=%laikas% /interactive /next:%diena%)
 if /i "%isjungimas%"=="T" (set isjungti=1) else (set isjungti=0)
-at %data% %~s0 planinis %isjungti% %kanalas% %trukme% %failas% > nul
+set /p laikas=Nurodykite irasymo pradzios laika (formatas: HH:MM:SS): 
+set /p daugkartinis=Daugkartinis irasymas? [T/N]: 
+if /i "%daugkartinis%"=="T" goto daugkartinis
+set /p data=Nurodykite irasymo data (formatas: yyyy/mm/dd): 
+set nustatymai=/SC ONCE /ST %laikas% /SD %data%
+goto scheduleris
+:daugkartinis
+echo Nurodykite savaites diena/dienas, kada bus vykdomas irasymas:
+echo MON - pirmadienis;
+echo TUE - antradienis;
+echo WED - treciadienis;
+echo THU - ketvirtadienis;
+echo FRI - penktadienis;
+echo SAT - sestadienis;
+echo SUN - sekmadienis.
+echo Jeigu nurodote keleta dienu, jas atskirkite kableliais (pvz.: MON,WED,FRI)
+set /p dienos=
+set nustatymai=/SC WEEKLY /D %dienos% /ST %laikas%
+:scheduleris
+schtasks /Create %nustatymai% /TN "FripTV rec (%failas%)" /TR "%~s0 planinis %isjungti% %kanalas% %trukme% %failas%" > nul
 echo Irasymas prasides tavo nurodytu laiku. Sis langas tau nebereikalingas.
 goto isejimas
 :planinis
