672f13f9b01b768b1520b8b8ee9664ad73dded83
Ignas Daukšas <ignasd@gmail.com>
2018-07-21 13:49:05 +0300

    v3.0.0-alpha

--- rec.bat
+++ rec.bat
@@ -1,16 +1,16 @@
 @echo off
 cd /d "%~dp0"
 cls
-title FripTV 0.18 rec script 2.3.1-final
+title Rec script 3.0.0-earlyAlpha VLC edition
 color 17
-if exist friptv.exe goto pradzia
-echo Nerastas friptv.exe
+if exist vlc.exe goto pradzia
+echo Nerastas vlc.exe
 goto isejimas
 :pradzia
 if "%1"=="1" goto planinis
 if "%1"=="n" goto planinis
-set /p kanalas=Kanalo numeris: 
-set /p failas=Failo pavadinimas (su galune): 
+set /p streamas=Pilnas streamo URL (pvz. udp://@225.2.2.1:1234): 
+set /p failas=Failo pavadinimas (su galune, negali buti tarpu): 
 set /p pabaiga=Nurodykite irasymo pabaigos laika (formatas: HH:MM:SS): 
 set /p uzmigdymas=Uzmigdyti kompiuteri po irasymo? [T/N]: 
 set /p pradeti=Pradeti dabar? [T/N]: 
@@ -37,26 +37,28 @@ echo Jeigu nurodote keleta dienu, jas atskirkite kableliais (pvz.: MON,WED,FRI)
 set /p dienos=
 set nustatymai=/SC WEEKLY /D %dienos% /ST %laikas%
 :scheduleris
-schtasks /Create %nustatymai% /TN "FripTV rec (%failas%)" /TR "'%~0' %kartai% %uzmigdyti% %kanalas% %failas% %pabaiga%">nul
+schtasks /Create %nustatymai% /TN "Rec script (%failas%)" /TR "'%~0' %kartai% %uzmigdyti% %streamas% %failas% %pabaiga%">nul
 echo Irasymas prasides tavo nurodytu laiku. Sis langas tau nebereikalingas.
 goto isejimas
 :planinis
 echo Planinis irasymas
 echo.
 echo %DATE% %TIME%
-echo.
-set kanalas=%3
-echo Kanalo numeris: %kanalas%
-set failas=%4
-echo Failo pavadinimas (su galune): %failas%
 set pabaiga=%5
+echo Baigsis: %pabaiga%
+echo.
+set streamas=%3
+echo Streamo URL: %streamas%
+set failas=%4
+echo Failo pavadinimas: %DATE%_%failas%
 if "%2"=="1" set uzmigdymas=T
-if "%1"=="1" schtasks /Delete /TN "FripTV rec (%failas%)" /F>nul
+if "%1"=="1" schtasks /Delete /TN "Rec script (%failas%)" /F>nul
 :irasymas
-schtasks /Create /SC DAILY /ST %pabaiga% /TN "FripTV rec stop (%failas%)" /TR "taskkill /IM friptv.exe /T /F">nul
+echo.
+schtasks /Create /SC DAILY /ST %pabaiga% /TN "Rec script stop (%failas%)" /TR "taskkill /FI 'WINDOWTITLE eq vlc.exe  -I dummy %streamas%*' /T /F">nul
 echo Pradedamas irasymas. Sekanti zinute pranes apie irasymo pabaiga.
-friptv.exe /silent /ch=%kanalas% /file=%failas%
-schtasks /Delete /TN "FripTV rec stop (%failas%)" /F>nul
+cmd /c vlc.exe -I dummy %streamas% --sout=file/ts:"D:\Recorder\%DATE%_%failas%"
+schtasks /Delete /TN "Rec script stop (%failas%)" /F>nul
 echo Irasymas baigtas.
 if /i not "%uzmigdymas%"=="T" goto isejimas
 echo Vykdoma kompiuterio uzmigdymo komanda.
