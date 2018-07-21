8676ffebb60a70a4867140c73495d4246942348b
Ignas Daukšas <ignasd@gmail.com>
2018-07-21 13:39:22 +0300

    v2.1.0-alpha

--- rec.bat
+++ rec.bat
@@ -1,7 +1,7 @@
 @echo off
 cd /d %~dps0
 cls
-title FripTV 0.18 rec script v2-alpha
+title FripTV 0.18 rec script v2.1-alpha
 color 17
 if exist friptv.exe goto pradzia
 echo Nerastas friptv.exe
@@ -11,10 +11,10 @@ if "%1"=="planinis" goto planinis
 set /p kanalas=Kanalo numeris: 
 set /p trukme=Iraso trukme (minutemis): 
 set /p failas=Failo pavadinimas (su galune): 
-set /p isjungimas=Isjungti kompiuteri po irasymo? [T/N]: 
+set /p uzmigdymas=Uzmigdyti kompiuteri po irasymo? [T/N]: 
 set /p pradeti=Pradeti dabar? [T/N]: 
 if /i "%pradeti%"=="T" goto irasymas
-if /i "%isjungimas%"=="T" (set isjungti=1) else (set isjungti=0)
+if /i "%uzmigdymas%"=="T" (set uzmigdyti=1) else (set uzmigdyti=0)
 set /p laikas=Nurodykite irasymo pradzios laika (formatas: HH:MM:SS): 
 set /p daugkartinis=Daugkartinis irasymas? [T/N]: 
 if /i "%daugkartinis%"=="T" goto daugkartinis
@@ -34,7 +34,7 @@ echo Jeigu nurodote keleta dienu, jas atskirkite kableliais (pvz.: MON,WED,FRI)
 set /p dienos=
 set nustatymai=/SC WEEKLY /D %dienos% /ST %laikas%
 :scheduleris
-schtasks /Create %nustatymai% /TN "FripTV rec (%failas%)" /TR "%~s0 planinis %isjungti% %kanalas% %trukme% %failas%" > nul
+schtasks /Create %nustatymai% /TN "FripTV rec (%failas%)" /TR "%~s0 planinis %uzmigdyti% %kanalas% %trukme% %failas%" > nul
 echo Irasymas prasides tavo nurodytu laiku. Sis langas tau nebereikalingas.
 goto isejimas
 :planinis
@@ -45,14 +45,14 @@ set trukme=%4
 echo Iraso trukme (minutemis): %trukme%
 set failas=%5
 echo Failo pavadinimas (su galune): %failas%
-if "%2"=="1" set isjungimas=T
+if "%2"=="1" set uzmigdymas=T
 :irasymas
 echo Pradedamas irasymas. Sekanti zinute pranes apie irasymo pabaiga.
 friptv.exe /silent /ch=%kanalas% /file=%failas% /t=%trukme%
 echo Irasymas baigtas.
-if /i not "%isjungimas%"=="T" goto isejimas
-echo Paleidziama kompiuterio isjungimo seka.
-shutdown -s -t 60 -c "Irasymas baigtas." -f
+if /i not "%uzmigdymas%"=="T" goto isejimas
+echo Vykdoma kompiuterio uzmigdymo komanda.
+rundll32 powrprof.dll,SetSuspendState
 :isejimas
 echo Spauskite betkoki klavisa, kad iseitumete.
 pause>nul
